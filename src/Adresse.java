ccfd53fa2e20edd46bca821c0ed7fab3ff175a79
Tabea Kaempf <tabea.kaempf@protonmail.com>
2024-08-21 19:44:26 +0200

    feat: Hausaufgabe gemeinsame Verwendung von Objekten

---
+++ src/Adresse.java
@@ -0,0 +1,8 @@
+class Adresse {
+  
+    String strasse = "von-Flotow-Str.";
+    String ort = "Marl";
+    int hausnummer;
+    int postleitzahl;
+
+}
---
+++ src/Person.java
@@ -0,0 +1,6 @@
+class Person {
+    String nachname = "NONAME";
+    String vorname = "NONAME";
+
+    Adresse anschrift;
+}
